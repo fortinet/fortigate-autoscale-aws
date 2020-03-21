7f155be42a59a1a5b126ac73aab3578406aed166
Jayden Liang <jliang01@fortinet.com>
2020-03-21 01:15:42 -0700

    bugfix, improvement, and add some more implementations
    Change-Id: Idaa72bad8e122beebc90343ebe636c75aebe5fb3

--- functions/fgt-as-handler/index.ts
+++ functions/fgt-as-handler/index.ts
@@ -1,12 +1,11 @@
 import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from 'aws-lambda';
 import {
-    FortiGateAutoscale,
     AutoscaleEnvironment,
     AwsLambdaProxy,
+    AwsPlatform,
     AwsPlatformAdapter,
-    AwsPlatform
+    FortiGateAutoscaleAws
 } from 'autoscale-core';
-import FortiGateAutoscaleAws from 'autoscale-core/fortigate-autoscale/aws/fortigate-autoscale-aws';
 export const handler = (
     event: APIGatewayProxyEvent,
     context: Context
@@ -14,26 +13,24 @@ export const handler = (
     const env = {} as AutoscaleEnvironment;
     const proxy = new AwsLambdaProxy(event, context);
     const platform = new AwsPlatformAdapter(new AwsPlatform(), proxy);
-    const autoscale = new FortiGateAutoscale<APIGatewayProxyEvent, Context, APIGatewayProxyResult>(
-        platform,
-        env,
-        proxy
-    );
-    return autoscale.handleRequest(proxy, platform, env);
+    const autoscale = new FortiGateAutoscaleAws(platform, env, proxy);
+    return autoscale.handleCloudFunctionRequest(proxy, platform, env);
 };
 
 export const handlerEx = (
     event: APIGatewayProxyEvent,
     context: Context
 ): [
-    FortiGateAutoscale<APIGatewayProxyEvent, Context, APIGatewayProxyResult>,
+    FortiGateAutoscaleAws,
     AutoscaleEnvironment,
+    AwsPlatform,
     AwsPlatformAdapter,
     AwsLambdaProxy
 ] => {
     const env = {} as AutoscaleEnvironment;
     const proxy = new AwsLambdaProxy(event, context);
-    const pa = new AwsPlatformAdapter(new AwsPlatform(), proxy);
+    const p = new AwsPlatform();
+    const pa = new AwsPlatformAdapter(p, proxy);
     const autoscale = new FortiGateAutoscaleAws(pa, env, proxy);
-    return [autoscale, env, pa, proxy];
+    return [autoscale, env, p, pa, proxy];
 };
--- scripts/make-dist.ts
+++ scripts/make-dist.ts
@@ -1,7 +1,13 @@
 import path from 'path';
 import CodePackman from './code-packman';
+
+const PROJECT_ROOT = path.resolve(path.dirname(__filename), '../');
+const FUNCTION_DIR = path.resolve(PROJECT_ROOT, './functions');
+/**
+ * build fgt-as-handler function
+ */
 (async () => {
-    const projectRoot = path.resolve(path.dirname(__filename), '../../');
+    const projectRoot = path.resolve(FUNCTION_DIR, 'fgt-as-handler');
     const outDir = path.resolve(projectRoot, 'out');
     const cpm = new CodePackman(projectRoot);
     await cpm.buildTypeScriptProject();
