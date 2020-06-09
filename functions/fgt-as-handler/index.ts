1ee85c7c55d4d246b7dfb42a05172afd6a9b6551
Jayden Liang <jliang01@fortinet.com>
2020-06-09 16:20:48 -0700

    initially added templates and merged tgw integration into them
    Change-Id: Ia051b17040424cba7b45bd01ddd78cc67872ab03

--- functions/fgt-as-handler/index.ts
+++ functions/fgt-as-handler/index.ts
@@ -1,12 +1,17 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context, ScheduledEvent } from 'aws-lambda';
-import { AwsPlatformAdapter, AwsFortiGateAutoscale, AwsPlatformAdaptee } from 'autoscale-core';
+import {
+    AwsPlatformAdapter,
+    AwsFortiGateAutoscale,
+    AwsPlatformAdaptee,
+    AwsFortiGateAutoscaleTgw
+} from 'autoscale-core';
 import { AutoscaleEnvironment } from 'autoscale-core/dist/autoscale-environment';
 import {
     AwsApiGatewayEventProxy,
     AwsScheduledEventProxy
 } from 'autoscale-core/dist/fortigate-autoscale/aws/aws-cloud-function-proxy';
 // API Gateway event handler for http requests coming from FortiGate callback
-export const handler = (
+export const autoscaleHandler = (
     event: APIGatewayProxyEvent,
     context: Context
 ): Promise<APIGatewayProxyResult> => {
@@ -22,11 +27,44 @@ export const handler = (
 };
 
 // to handle cloudwatch scheduled event
-export const scheduledEventhandler = (event: ScheduledEvent, context: Context): Promise<void> => {
+export const scheduledEventHandler = (event: ScheduledEvent, context: Context): Promise<void> => {
     const env = {} as AutoscaleEnvironment;
     const proxy = new AwsScheduledEventProxy(event, context);
     const platform = new AwsPlatformAdapter(new AwsPlatformAdaptee(), proxy);
     const autoscale = new AwsFortiGateAutoscale<ScheduledEvent, Context, void>(
+        platform,
+        env,
+        proxy
+    );
+    autoscale.handleAutoscaleRequest(proxy, platform, env);
+    return Promise.resolve();
+};
+
+// Transit Gateway Integration
+// API Gateway event handler for http requests coming from FortiGate callback
+export const autoscaleTgwHandler = (
+    event: APIGatewayProxyEvent,
+    context: Context
+): Promise<APIGatewayProxyResult> => {
+    const env = {} as AutoscaleEnvironment;
+    const proxy = new AwsApiGatewayEventProxy(event, context);
+    const platform = new AwsPlatformAdapter(new AwsPlatformAdaptee(), proxy);
+    const autoscale = new AwsFortiGateAutoscaleTgw<
+        APIGatewayProxyEvent,
+        Context,
+        APIGatewayProxyResult
+    >(platform, env, proxy);
+    return autoscale.handleAutoscaleRequest(proxy, platform, env);
+};
+
+export const scheduledEventTgwHandler = (
+    event: ScheduledEvent,
+    context: Context
+): Promise<void> => {
+    const env = {} as AutoscaleEnvironment;
+    const proxy = new AwsScheduledEventProxy(event, context);
+    const platform = new AwsPlatformAdapter(new AwsPlatformAdaptee(), proxy);
+    const autoscale = new AwsFortiGateAutoscaleTgw<ScheduledEvent, Context, void>(
         platform,
         env,
         proxy
