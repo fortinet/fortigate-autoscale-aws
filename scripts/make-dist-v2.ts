db6a03cf5c67ccf9ec2e0987a63f4dab0e5ba242
Jayden Liang <jliang01@fortinet.com>
2020-06-16 13:06:47 -0700

    update make-dist (v2) script to depend on tsconfig and packman.config
    also change again the build and dist output directories
    
    Change-Id: I160d98c9e6aff6fe9d5d07d21eba1142cacd0d01

--- scripts/make-dist-v2.ts
+++ scripts/make-dist-v2.ts
@@ -1,13 +1,30 @@
 import path from 'path';
+import fs from 'fs';
 import { Command } from 'commander';
+import { parse as CommentJsonParse } from 'comment-json';
 import { CodePackman } from 'autoscale-core';
 
-const REAL_PROJECT_ROOT = path.resolve(__dirname, '../');
-const cpm = new CodePackman(REAL_PROJECT_ROOT, './dist');
+let projectRoot: string;
+let tsConfigJson: {
+    compilerOptions: { outDir: string; [key: string]: unknown };
+    [key: string]: unknown;
+};
+let cpm: CodePackman;
+
+const init = (command: Command): Promise<void> => {
+    projectRoot = path.resolve(command.projectRoot);
+    console.log(projectRoot, command);
+    tsConfigJson = CommentJsonParse(
+        fs.readFileSync(path.resolve(projectRoot, 'tsconfig.json')).toString()
+    );
+    console.log(tsConfigJson);
+    cpm = new CodePackman(projectRoot, tsConfigJson.compilerOptions.outDir);
+    return Promise.resolve();
+};
 
 const makeDistAwsCloudFormation = async (): Promise<boolean> => {
     console.info('Making distribution zip package for: AWS Cloud Formation');
-    cpm.projectRoot = path.resolve(cpm.projectRoot, '../');
+    // cpm.projectRoot = path.resolve(cpm.projectRoot, '../');
     try {
         await cpm.buildWithPackmanConfig();
         return true;
@@ -22,14 +39,18 @@ program.description('( ͡° ͜ʖ ͡°) FortiGate Autoscale make dist script.');
 program
     .command('build-aws-cloudformation')
     .description('build aws cloudformation deployment package.')
-    .action(async () => {
+    .requiredOption('--project-root <value>', 'set the project root to run this script.')
+    .action(async command => {
+        await init(command);
         await makeDistAwsCloudFormation();
     });
 
 program
     .command('build-all')
     .description('run all build commands, each at a time.')
-    .action(async () => {
+    .requiredOption('--project-root <value>', 'set the project root to run this script.')
+    .action(async command => {
+        await init(command);
         await makeDistAwsCloudFormation();
     });
 
